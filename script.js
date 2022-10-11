c0998d35262fdbaacbbf20c219159ddcfd215f05
Rihan soares <rihan145@gmail.com>
2022-10-11 15:45:03 -0300

    entrega concluida

---
+++ script.js
@@ -0,0 +1,74 @@
+//            1
+// let valorDaCompra = parseFloat(prompt("Digite o valor que deseja abastecer."));
+// let valorLitro = parseFloat(6.80);
+// let litros = valorDaCompra/valorLitro;
+// alert(`O cliente abasteceu ${litros.toFixed(2)} litros`);
+
+
+//            2
+// let valorCelsius = parseFloat(prompt("Digite o valor da temperatura atual."));
+// let valorFahrenheit = parseFloat((9 * valorCelsius + 160) / 5);
+// alert(`O valor ${valorCelsius.toFixed(1)}ºc em Fahrenheit é ${valorFahrenheit.toFixed(1)} .`);
+
+
+//            3
+// let valorFahrenheit = parseFloat(prompt("Digite o valor da temperatura atual."));
+// let valorCelsius =parseFloat((valorFahrenheit - 32) * 5 / 9);
+// alert(`O valor ${valorFahrenheit.toFixed(1)}ºf em Celsius é ${valorCelsius} .`);
+
+
+//            4
+// let comprimento = parseInt(prompt("Digite o Comprimento da caixa."));
+// let largura = parseInt(prompt("Digite a largura da caixa."));
+// let altura = parseInt(prompt("Digite a altura da caixa."));
+// let volume = comprimento * largura * altura;
+// alert(`O volume é de: ${volume}m³`);
+
+
+//            5
+// let valorInt = parseInt(prompt("Digite um numero"));
+// let valorQuadrado = valorInt ** 2;
+// alert(`O valor ${valorInt} ao quadrado é ${valorQuadrado}`);
+
+
+//            6
+// let nota1 = parseFloat(prompt("Qual a primeira nota ?"));
+// let nota2 = parseFloat(prompt("Qual a segunda nota ?"));
+// let nota3 = parseFloat(prompt("Qual a terceira nota ?"));
+// let nota4 = parseFloat(prompt("Qual a quarta nota ?"));
+// let nota5 = parseFloat(prompt("Qual a quinta nota ?"));
+// let notaSoma =parseFloat(nota1 + nota2 + nota3 + nota4 + nota5);
+// let media = parseFloat(notaSoma / 5);
+// alert(`A soma dos valores informados é: ${notaSoma} e a média das notas é: ${media}`);
+
+
+//              7
+// let numero1 = parseInt(prompt("Digite o primeiro numero."));
+// let numero2 = parseInt(prompt("Digite o segundo numero."));
+// let restoDivisao = numero1 % numero2;
+// alert(`O resto da divisão de ${numero1} por ${numero2} é: ${restoDivisao}`);
+
+
+//              8
+// let texto = prompt("Digite o texto aqui.");
+// let textoCaracteres = texto.length;
+// alert(`Este texto tem ${textoCaracteres} caracteres`);
+
+
+//              9
+// let texto = prompt("Digite o texto aqui.");
+// let primeiraLetra = texto[0].toUpperCase();
+// let ultimaLetra = texto[texto.length -1].toLowerCase();
+// alert(`A primeira letra deste texto é ${primeiraLetra} e última é ${ultimaLetra}`);
+
+
+
+//                10
+// let nome  = prompt("Digite o seu primeiro nome.");
+// let sobrenome  = prompt("Digite o seu sobrenome.");
+// let idade = parseInt(prompt("Digite a sua idade"));
+// let setor = prompt("Digite o seu setor");
+// let nomeMaiusculo = `${nome[0].toUpperCase()}${nome.slice(1,)}`;
+// let sobrenomeMaiusculo = `${sobrenome[0].toUpperCase()}${sobrenome.slice(1,)}`;
+// let setorMaiusculo = `${setor[0].toUpperCase()}${setor.slice(1,)}`;
+// alert(`nome: ${nomeMaiusculo} ${sobrenomeMaiusculo} - idade: ${idade} - Setor: ${setorMaiusculo}`);
